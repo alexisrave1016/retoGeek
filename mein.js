4429286d804b3ee232e9aa99c0a52378a8ec771f
alexisrave1016 <kiarepuestos.medellin@gmail.com>
2021-07-19 00:17:37 -0500

    add files

---
+++ mein.js
@@ -0,0 +1,799 @@
+//let datos = new Array();
+
+let datos = [{
+        Propiedad: 1,
+        metros: 55,
+        tipo: "apt",
+        habitantes: [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }]
+    },
+
+    {
+        Propiedad: 2,
+        metros: 65,
+        tipo: "casa",
+        habitantes: [{
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 10,
+                "sexo": "F"
+            },
+            {
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }
+        ]
+    },
+    {
+        Propiedad: 3,
+        metros: 45,
+        tipo: "casa",
+        habitantes: [{
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 10,
+                "sexo": "F"
+            },
+            {
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }
+        ]
+    },
+
+    {
+        "Propiedad": 4,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 5,
+        "metros": 55,
+        "tipo": "apt",
+
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 6,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 10,
+                "sexo": "F"
+            },
+            {
+                "edad": 25,
+                "sexo": "M"
+            }, {
+                "edad": 34,
+                "sexo": "M"
+            }, {
+                "edad": 11,
+                "sexo": "F"
+            }
+        ]
+    },
+
+
+    {
+        "Propiedad": 7,
+        "metros": 55,
+        "tipo": "apt",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }]
+    },
+
+    {
+        "Propiedad": 8,
+        "metros": 65,
+        "tipo": "casa",
+
+        "habitantes": [{
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 10,
+                "sexo": "F"
+            },
+            {
+                "edad": 45,
+                "sexo": "F"
+            }, {
+                "edad": 50,
+                "sexo": "M"
+            }, {
+                "edad": 18,
+                "sexo": "F"
+            },
+            {
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 31,
+                "sexo": "M"
+            }, {
+                "edad": 14,
+                "sexo": "F"
+            }
+        ]
+    },
+
+    {
+        "Propiedad": 9,
+        "metros": 50,
+        "tipo": "apt",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 15,
+            "sexo": "M"
+        }]
+    },
+    {
+        "Propiedad": 10,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 11,
+        "metros": 55,
+        "tipo": "apt",
+
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 25,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 12,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 13,
+        "metros": 55,
+        "tipo": "apt",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 14,
+        "metros": 65,
+        "tipo": "casa",
+
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 35,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 15,
+        "metros": 50,
+        "tipo": "apt",
+        "habitantes": [{
+                "edad": 25,
+                "sexo": "F"
+            }, {
+                "edad": 30,
+                "sexo": "M"
+            }, {
+                "edad": 10,
+                "sexo": "F"
+            }, {
+                "edad": 75,
+                "sexo": "F"
+            },
+            {
+                "edad": 70,
+                "sexo": "M"
+            }
+        ]
+    },
+
+    {
+        "Propiedad": 16,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 17,
+        "metros": 55,
+        "tipo": "apt",
+
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 18,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 65,
+            "sexo": "F"
+        }]
+    },
+
+    {
+        "Propiedad": 19,
+        "metros": 50,
+        "tipo": "apt",
+
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 25,
+            "sexo": "M"
+        }]
+    },
+
+    {
+        "Propiedad": 20,
+        "metros": 65,
+        "tipo": "casa",
+        "habitantes": [{
+            "edad": 25,
+            "sexo": "F"
+        }, {
+            "edad": 30,
+            "sexo": "M"
+        }, {
+            "edad": 10,
+            "sexo": "F"
+        }, {
+            "edad": 26,
+            "sexo": "M"
+        }]
+    }
+]
+
+
+
+const cobro = document.getElementsByClassName('cobro')[0];
+console.log(cobro);
+
+function crearPlanilla(datos) {
+    const planilla = document.createElement('div');
+    planilla.className = "planilla";
+
+    ////  creacion de columnas 
+
+    const colum1 = document.createElement('div');
+    colum1.className = "colum1"
+    const colum2 = document.createElement('div');
+    colum2.className = "colum2"
+    const colum3 = document.createElement('div');
+    colum3.className = "colum3"
+    const colum4 = document.createElement('div');
+    colum4.className = "colum4"
+
+    //// lo que va dentro de columna 1
+
+    const tituloPropiedad = document.createElement('div');
+    tituloPropiedad.innerText = "Señores Propiedad";
+
+    const valorAdmin = document.createElement('div');
+    valorAdmin.innerText = "Valor Administracion";
+
+    const valorAseo = document.createElement('div');
+    valorAseo.innerText = "Valor Cuota de Aseo";
+
+    const gym = document.createElement('div');
+    gym.innerText = "Valor Cuota de Aseo";
+
+
+    //// lo que va dentro de columna 2
+
+
+    const topNumero = document.createElement('div');
+    topNumero.innerText = datos.Propiedad;
+
+    //// lo que va dentro de columna 3
+
+    const textCuota = document.createElement('div');
+    textCuota.innerText = "Cuota mes de:";
+
+    //// lo que va dentro de columna 4
+
+
+    const resultAdmin = document.createElement('div');
+    resultAdmin.innerText = calcularAdmin(datos.metros, datos.tipo);
+
+
+
+    const resultAseo = document.createElement('div');
+    resultAseo.innerText = parseInt(resultAdmin.innerText) * 0.1 + (datos.metros * 1000);
+
+    //////   gym
+    const resultGym = document.createElement('div');
+    resultGym.innerText = calcularGym(datos.habitantes);
+
+
+    planilla.append(colum1);
+    planilla.append(colum2);
+    planilla.append(colum3);
+    planilla.append(colum4);
+
+
+    /////   colum 1
+
+    colum1.append(tituloPropiedad);
+    colum1.append(valorAdmin);
+    colum1.append(valorAseo);
+    colum1.append(gym);
+
+
+    ////// colum 2
+
+    colum2.append(topNumero);
+
+
+    ///// colum 3
+
+    colum3.append(textCuota)
+
+    //// colum 4
+
+    colum4.append(resultAdmin)
+    colum4.append(resultAseo)
+    colum4.append(resultGym)
+
+
+
+    return planilla;
+}
+
+
+
+datos.forEach(elem => {
+
+    cobro.appendChild(crearPlanilla(elem));
+
+})
+
+
+
+function calcularAdmin(metros, tipo) {
+
+    let num = null;
+
+    if (tipo == "casa") {
+
+        let num = 100000
+
+    } else if (tipo == "apt") {
+
+        let num = 50000
+    }
+
+    let result = metros * 1500 + num
+    //console.log(result)
+    return result
+}
+
+function calcularGym(habitantes) {
+    let valorgym = null;
+
+    habitantes.forEach(elem => {
+        if (elem.sexo == "F") {
+            if (elem.edad < 10 || elem.edad >= 55) {
+                valorgym = valorgym + 0
+            } else if (elem.edad >= 10 && elem.edad < 18) {
+                valorgym = valorgym + 15000
+            } else if (elem.edad >= 18 && elem.edad < 35) {
+                valorgym = valorgym + 12000
+            } else if (elem.edad >= 35 && elem.edad < 55) {
+                valorgym = valorgym + 8000
+            }
+        } else if (elem.sexo == "M") {
+            if (elem.edad < 10 || elem.edad >60) {
+                valorgym = valorgym + 0
+            } else if (elem.edad >= 10 && elem.edad < 20) {
+                valorgym = valorgym + 20000
+            } else if (elem.edad >= 20 && elem.edad < 40) {
+                valorgym = valorgym + 15000
+            } else if (elem.edad >= 40 && elem.edad < 60) {
+                valorgym = valorgym + 10000
+            }
+
+        }
+
+
+    })
+
+    return valorgym;
+    console.log(valorgym)
+}
+
+
+
+
+/*
+
+let tablaProducto = document.getElementById('cobro1');
+let cuerpoTabla = document.createElement('tbody');
+
+datos.forEach(p => {
+    console.log(p.Propiedad);
+
+    let fila = document.createElement('tr');
+    let td = document.createElement('td');
+
+    td.innerText = p.Propiedad;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.metros;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.tipo;
+    fila.appendChild(td);
+
+    cuerpoTabla.appendChild(fila);
+})
+
+
+tablaProducto.appendChild(cuerpoTabla);
+
+*/
+
+/*const cobro = document.getElementsByClassName('cobro')[0];
+console.log(cobro);
+
+function crearPlanilla(numero){
+    const planilla = document.createElement('div');
+    planilla.className = "planilla";
+
+    const topNumero = document.createElement('div');
+    topNumero.innerText= numero;
+
+    const bottomNumero = document.createElement('div');
+    bottomNumero.className = "right";
+    bottomNumero.innerText = numero;
+
+    planilla.append(topNumero);
+    planilla.append(bottomNumero);
+
+    return planilla;
+}
+
+cobro.appendChild(crearPlanilla("mundo eres el",));
+cobro.appendChild(crearPlanilla(10 ,1));
+cobro.appendChild(crearPlanilla(15 ,1));
+cobro.appendChild(crearPlanilla(25,2));
+
+*/
+
+//------------------------------------------------------------------------------------------
+
+
+/*const cuuerpo = document.querySelector("body")
+
+const datos = [
+    
+{"Propiedad": 1, 
+"metros": 55,
+"tipo": "apt", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"}]     
+},
+
+{"Propiedad": 2, 
+"metros": 65, 
+"tipo": "casa", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},
+{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"}]     
+},
+
+{"Propiedad": 3, 
+"metros": 50, 
+"tipo": "apt", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 4, 
+"metros": 65,
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 5, 
+"metros": 55, 
+"tipo": "apt", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 6, 
+"metros": 65, 
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},
+{"edad":25, "sexo": "M"},{"edad":34, "sexo": "M"},{"edad":11, "sexo": "F"}]     
+},
+
+
+{"Propiedad": 7, 
+"metros": 55,
+"tipo": "apt", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":30, "sexo": "M"}]     
+},
+
+{"Propiedad": 8, 
+"metros": 65, 
+"tipo": "casa", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},
+{"edad":45, "sexo": "F"},{"edad":50, "sexo": "M"},{"edad":18, "sexo": "F"},
+{"edad":25, "sexo": "F"},{"edad":31, "sexo": "M"},{"edad":14, "sexo": "F"}]     
+},
+
+{"Propiedad": 9, 
+"metros": 50, 
+"tipo": "apt", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":15, "sexo": "M"}]     
+},
+{"Propiedad": 10, 
+"metros": 65,
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 11, 
+"metros": 55, 
+"tipo": "apt", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":25, "sexo": "F"}]     
+},
+
+{"Propiedad": 12, 
+"metros": 65, 
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 13, 
+"metros": 55,
+"tipo": "apt", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 14, 
+"metros": 65, 
+"tipo": "casa", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":35, "sexo": "F"}]     
+},
+
+{"Propiedad": 15, 
+"metros": 50, 
+"tipo": "apt", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":75, "sexo": "F"},
+{"edad":70, "sexo": "M"}]     
+},
+
+{"Propiedad": 16, 
+"metros": 65,
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 17, 
+"metros": 55, 
+"tipo": "apt", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"}]     
+},
+
+{"Propiedad": 18, 
+"metros": 65, 
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":65, "sexo": "F"}]     
+},
+
+{"Propiedad": 19, 
+"metros": 50, 
+"tipo": "apt", 
+
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":25, "sexo": "M"}]     
+},
+
+{"Propiedad": 20, 
+"metros": 65, 
+"tipo": "casa", 
+"habitantes":[{"edad":25, "sexo": "F"},{"edad":30, "sexo": "M"},{"edad":10, "sexo": "F"},{"edad":26, "sexo": "M"}]     
+},
+
+
+
+
+
+
+]
+/*
+datos.forEach(elem => {
+ const datos = document.createElement("DIV")
+
+ let numeroHabitantes = elem.habitantes.length;
+
+ datos.innerHTML = "<h1>"+numeroHabitantes+"</h1>";
+
+ 
+//console.log(elem.numeroCasa)
+cuuerpo.appendChild(datos)
+
+})*/
